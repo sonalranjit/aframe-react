2f2d46749426a5b1d80f825d1fdcad0c1fddb7f5
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-06-06 14:31:32 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
+import {Entity, Scene} from 'aframe-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  
+  render () {
+    return (
+      <Scene>
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
+        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        <a-sky color="#ECECEC"></a-sky>
+      </Scene>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#sceneContainer'));
