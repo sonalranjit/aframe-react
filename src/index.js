af079dafccf86ccb38a919d9b4f129dbd5d99103
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-06-06 17:25:21 -0400

    changed aframe to a class

--- src/index.js
+++ src/index.js
@@ -8,18 +8,20 @@ import ReactDOM from 'react-dom';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon, Button} from 'react-bootstrap';
 
 
-function Aframe() {
-  return(
-    <div>
-      <Scene>
-          <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
-          <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-          <a-sky color="#ECECEC"></a-sky>
-      </Scene>
-    </div>
-  );
+class Aframe extends React.Component {
+  render () {
+    return(
+      <div>
+        <Scene>
+            <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+            <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+            <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
+            <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+            <a-sky color="#ECECEC"></a-sky>
+        </Scene>
+      </div>
+    );
+  }
 }
 
 class App extends React.Component {
@@ -34,17 +36,6 @@ class App extends React.Component {
                 <Glyphicon glyph="menu-left"/>
               </Navbar.Brand>
             </Navbar.Header>
-            <Nav>
-              <NavItem eventKey={1} href="#">Link</NavItem>
-              <NavItem eventKey={2} href="#">Link</NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.4}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
           </Navbar>
         </div>
         <Aframe />
