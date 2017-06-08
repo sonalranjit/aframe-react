6112f18bf4649c076aec4e29dda11111437d809a
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-06-07 21:20:14 -0400

    added navbar to the aframe scene

--- src/index.js
+++ src/index.js
@@ -9,26 +9,6 @@ import {Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon, Button, Grid, Ro
 DropdownButton, Jumbotron} from 'react-bootstrap';
 import './index.css';
 
-class App extends React.Component {
-  
-  render () {
-    return (
-      <div>
-        <div>
-          <Navbar>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <Glyphicon glyph="menu-left"/>
-              </Navbar.Brand>
-            </Navbar.Header>
-          </Navbar>
-        </div>
-        <Aframe />
-      </div>
-    );
-  }
-}
-
 class AodaPage extends React.Component{
   constructor(props){
     super(props);
@@ -93,13 +73,26 @@ function LandingPage(props){
 
 function Aframe(){
   return(
-    <Scene>
-        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
-        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-        <a-sky color="#ECECEC"></a-sky>
-    </Scene>
+    <div>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Glyphicon glyph="menu-left"/>
+            </Navbar.Brand>
+          </Navbar.Header>
+        </Navbar>
+      </div>
+      <div>
+      <Scene>
+          <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
+          <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+          <a-sky color="#ECECEC"></a-sky>
+      </Scene>
+      </div>
+    </div>
   );
 }
 
