5dfaad0cdc709e49dfa748130a01dba56fa285fd
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-06-08 19:19:00 -0400

    added navbar to aframe scene

--- src/index.js
+++ src/index.js
@@ -31,7 +31,7 @@ class AodaPage extends React.Component{
 function ViewRenderer(props){
   const isAframe = props.isAframe;
   if(isAframe){
-    return <Aframe />;
+    return <AframeNavbar />;
   }else{
     return <LandingPage onClick={props.showFrame}/>;
   }
@@ -73,25 +73,38 @@ function LandingPage(props){
 
 function Aframe(){
   return(
+    <Scene>
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
+        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        <a-sky color="#ECECEC"></a-sky>
+    </Scene>
+  );
+}
+
+function AframeNavbar(){
+  return(
     <div>
-      <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <Glyphicon glyph="menu-left"/>
-            </Navbar.Brand>
-          </Navbar.Header>
-        </Navbar>
-      </div>
-      <div>
+    <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Glyphicon glyph="menu-left"/>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1}>AODA Call for Action</NavItem>
+          <NavItem eventKey={2}>Office Space</NavItem>
+        </Nav>
+      </Navbar>
+
       <Scene>
-          <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-          <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-          <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
-          <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-          <a-sky color="#ECECEC"></a-sky>
-      </Scene>
-      </div>
+        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
+        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
+        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
+        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
+        <a-sky color="#ECECEC"></a-sky>
+    </Scene>
     </div>
   );
 }
