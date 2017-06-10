2a9cb853e5ed32dc19ea3804703bab84ca0ac83b
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-06-10 13:52:41 -0400

    obj loading working

--- src/index.js
+++ src/index.js
@@ -96,14 +96,18 @@ function AframeNavbar(){
           <NavItem eventKey={1}>AODA Call for Action</NavItem>
           <NavItem eventKey={2}>Office Space</NavItem>
         </Nav>
-      </Navbar>
+    </Navbar>
 
-      <Scene>
-        <a-box position="-1 0.5 -3" rotation="0 45 0" color="#4CC3D9"></a-box>
-        <a-sphere position="0 1.25 -5" radius="1.25" color="#EF2D5E"></a-sphere>
-        <a-cylinder position="1 0.75 -3" radius="0.5" height="1.5" color="FFC65D"></a-cylinder>
-        <a-plane position="0 0 -4" rotation="-90 0 0" width="4" height="4" color="#7BC8A4"></a-plane>
-        <a-sky color="#ECECEC"></a-sky>
+    <Scene>
+      <a-assets>
+        <a-asset-item id="cubicle-obj" src="models/cubicle_room.obj"></a-asset-item>
+        <a-asset-item id="cubicle-mtl" src="models/cubicle_room.mtl"></a-asset-item>
+      </a-assets>
+      <a-entity obj-model="obj: #cubicle-obj; mtl: #cubicle-mtl;" scale="1 1 1"
+        rotation="0 0 0"
+        position="1 1 1">
+      </a-entity>        
+      <a-sky color="#ECECEC"></a-sky>
     </Scene>
     </div>
   );
